a352904d86d7a7608b70e29050f9b29eca25dde7
Rick Ucker <rick.ucker@gmail.com>
2013-10-06 23:54:15 -0400

    Added time.h

---
+++ src/time.h
@@ -0,0 +1,26 @@
+//Time constants
+const char* const TIME_ONE	= "one";
+const char* const TIME_TWO      = "two";
+const char* const TIME_THREE	= "three";
+const char* const TIME_FOUR	= "four";
+const char* const TIME_FIVE	= "five";
+const char* const TIME_SIX	= "six";
+const char* const TIME_SEVEN	= "seven";
+const char* const TIME_EIGHT	= "eight";
+const char* const TIME_NINE	= "nine";
+const char* const TIME_TE	= "ten";
+const char* const TIME_ELEVEN	= "eleven";
+const char* const TIME_TWELVE	= "twelve";
+const char* const TIME_THIRTEEN	= "thirteen";
+const char* const TIME_FOURTEEN	= "fourteen";
+const char* const TIME_FIFTEEN	= "fifteen";
+const char* const TIME_SIXTEEN	= "sixteen";
+const char* const TIME_SEVENTEEN	= "seventeen";
+const char* const TIME_EIGHTEEN	= "eighteen";
+const char* const TIME_NINETEEN	= "nineteen";
+const char* const TIME_TWENTY	= "twenty";
+const char* const TIME_THIRTY	= "thirty";
+const char* const TIME_FORTY	= "forty";
+const char* const TIME_FIFTY	= "fifty";
+
+
