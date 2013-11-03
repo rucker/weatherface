ad8974576a8d0fb719baeaa865b8ac78e38f22cf
Rick Ucker <rick.ucker@gmail.com>
2013-11-03 13:17:50 -0500

    Remove ACCESS_FINE_LOCATION permission. Split retrieval of weather info and Pebble communication to AsyncTask.

--- android/src/com/rucker/weatherface/android/MainActivity.java
+++ android/src/com/rucker/weatherface/android/MainActivity.java
@@ -1,13 +1,5 @@
 package com.rucker.weatherface.android;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.UUID;
-
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
@@ -19,10 +11,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
-import com.getpebble.android.kit.PebbleKit;
-import com.getpebble.android.kit.util.PebbleDictionary;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.rucker.weatherface.asynctask.GetWeatherInfoTask;
 
 /**
  * @see https://github.com/pebble/pebblekit/blob/master/Android/PebbleKitExample/src/com/example/PebbleKitExample/ExampleWeatherActivity.java
@@ -31,15 +22,8 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
  */
 public class MainActivity extends Activity {
 	
-    // the tuple key corresponding to the weather icon displayed on the watch
-    private static final int ICON_KEY = 0;
-    // the tuple key corresponding to the temperature displayed on the watch
-    private static final int TEMP_KEY = 1;
-    // This UUID identifies the weather app
-    private static final UUID WEATHER_UUID = UUID.fromString("E23101B0-F418-4F72-8709-FC90799FEE98");
     // Tag used for logging
     private static final String TAG = "Weatherface";
-
     private LocationManager mLocationManager;
     //ID for requests from this activity.
     private static final int activityRequestCode = 1;
@@ -57,7 +41,8 @@ public class MainActivity extends Activity {
 
     private boolean doGooglePlayServicesCheck() {
     	int playSvcAvail =  GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-		if (playSvcAvail != ConnectionResult.SUCCESS) { 
+    	Log.i(TAG, "Google Play Services status=" + playSvcAvail);
+		if (playSvcAvail != ConnectionResult.SUCCESS && playSvcAvail != ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED) { 
 			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(playSvcAvail, this, activityRequestCode);
 			errorDialog.show();
 			return false;
@@ -94,23 +79,11 @@ public class MainActivity extends Activity {
 
 //        mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         for (String provider : mLocationManager.getAllProviders()) {
-        	mLocationManager.requestLocationUpdates(provider, 0, 0, locationListener);
-        	//TODO shouldn't these be enumerated somewhere in the API?
-        	if (provider.equals("gps")) {
-//        		doWeatherUpdate(mLocationManager.getLastKnownLocation(provider));
+        	if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+        		mLocationManager.requestLocationUpdates(provider, 0, 0, locationListener);
+        		doWeatherUpdate(mLocationManager.getLastKnownLocation(provider));
         	}
         }
-    }
-
-    public void sendWeatherDataToWatch(int weatherIconId, int temperatureCelsius) {
-        // Build up a Pebble dictionary containing the weather icon and the current temperature in degrees celsius
-        PebbleDictionary data = new PebbleDictionary();
-        data.addUint8(ICON_KEY, (byte) weatherIconId);
-        data.addString(TEMP_KEY, String.format("%d\u00B0C", temperatureCelsius));
-
-        // Send the assembled dictionary to the weather watch-app; this is a no-op if the app isn't running or is not
-        // installed
-        PebbleKit.sendDataToPebble(getApplicationContext(), WEATHER_UUID, data);
     }
     
     /**
@@ -119,54 +92,7 @@ public class MainActivity extends Activity {
      */
 
     public void doWeatherUpdate(Location location) {
-        // A very sketchy, rough way of getting the local weather forecast from the phone's approximate location
-        // using the OpenWeatherMap webservice: http://openweathermap.org/wiki/API/JSON_API
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        
-        Log.i(TAG, "Found latitude of " + latitude + " and longitude of " + longitude);
-
-        try {
-            URL u = new URL(String.format("http://api.openweathermap.org/data/2.1/find/city?lat=%f&lon=%f&cnt=1",
-                    latitude,
-                    longitude));
-
-            HttpURLConnection urlConnection = (HttpURLConnection) u.openConnection();
-            try {
-                BufferedReader reader =
-                        new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-                String json = reader.readLine();
-
-                JSONObject jsonObject = new JSONObject(json);
-                JSONObject l = jsonObject.getJSONArray("list").getJSONObject(0);
-                JSONObject m = l.getJSONObject("main");
-                double temperature = m.getDouble("temp");
-                int wtype = l.getJSONArray("weather").getJSONObject(0).getInt("id");
-
-                int weatherIcon = getIconFromWeatherId(wtype);
-                int temp = (int) (temperature - 273.15);
-
-                sendWeatherDataToWatch(weatherIcon, temp);
-            } finally {
-                urlConnection.disconnect();
-            }
-
-            Log.d("WeatherActivity", String.format("%f, %f", latitude, longitude));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    	GetWeatherInfoTask gwit = new GetWeatherInfoTask(getApplicationContext());
+    	gwit.execute(location);
     }
-
-    private int getIconFromWeatherId(int weatherId) {
-        if (weatherId < 600) {
-            return 2;
-        } else if (weatherId < 700) {
-            return 3;
-        } else if (weatherId > 800) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
-    
 }
---
+++ android/src/com/rucker/weatherface/asynctask/GetWeatherInfoTask.java
@@ -0,0 +1,97 @@
+package com.rucker.weatherface.asynctask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+public class GetWeatherInfoTask extends AsyncTask<Location, Void, Void> {
+	
+    // the tuple key corresponding to the weather icon displayed on the watch
+    private static final int ICON_KEY = 0;
+    // the tuple key corresponding to the temperature displayed on the watch
+    private static final int TEMP_KEY = 1;
+    // This UUID identifies the weather app
+    private static final UUID WEATHER_UUID = UUID.fromString("E23101B0-F418-4F72-8709-FC90799FEE98");
+	private static final String TAG = "GetWeatherInfoTask";
+	private Context ctx;
+	
+	public GetWeatherInfoTask(Context ctx) {
+		this.ctx = ctx;
+	}
+	
+	@Override
+	protected Void doInBackground(Location... params) {
+		   // A very sketchy, rough way of getting the local weather forecast from the phone's approximate location
+        // using the OpenWeatherMap webservice: http://openweathermap.org/wiki/API/JSON_API
+        double latitude = params[0].getLatitude();
+        double longitude = params[0].getLongitude();
+        
+        Log.i(TAG, "Found latitude of " + latitude + " and longitude of " + longitude);
+
+        try {
+            URL u = new URL(String.format("http://api.openweathermap.org/data/2.1/find/city?lat=%f&lon=%f&cnt=1",
+                    latitude,
+                    longitude));
+
+            HttpURLConnection urlConnection = (HttpURLConnection) u.openConnection();
+            try {
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+                String json = reader.readLine();
+
+                JSONObject jsonObject = new JSONObject(json);
+                JSONObject l = jsonObject.getJSONArray("list").getJSONObject(0);
+                JSONObject m = l.getJSONObject("main");
+                double temperature = m.getDouble("temp");
+                int wtype = l.getJSONArray("weather").getJSONObject(0).getInt("id");
+
+                int weatherIcon = getIconFromWeatherId(wtype);
+                int temp = (int) (temperature - 273.15);
+
+                sendWeatherDataToWatch(weatherIcon, temp);
+            } finally {
+                urlConnection.disconnect();
+            }
+
+            Log.d("WeatherActivity", String.format("%f, %f", latitude, longitude));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+		return null;
+	}
+	
+    private int getIconFromWeatherId(int weatherId) {
+        if (weatherId < 600) {
+            return 2;
+        } else if (weatherId < 700) {
+            return 3;
+        } else if (weatherId > 800) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public void sendWeatherDataToWatch(int weatherIconId, int temperatureCelsius) {
+        // Build up a Pebble dictionary containing the weather icon and the current temperature in degrees celsius
+        PebbleDictionary data = new PebbleDictionary();
+        data.addUint8(ICON_KEY, (byte) weatherIconId);
+        data.addString(TEMP_KEY, String.format("%d\u00B0C", temperatureCelsius));
+
+        // Send the assembled dictionary to the weather watch-app; this is a no-op if the app isn't running or is not
+        // installed
+        PebbleKit.sendDataToPebble(ctx, WEATHER_UUID, data);
+    }
+}
